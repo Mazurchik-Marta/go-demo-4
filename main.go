921922168c2ac67cc0be594cadb81346802c7e86
user <martasiki@gmail.com>
2025-01-15 12:25:41 +0300

    Функция генератор паролей

--- main.go
+++ main.go
@@ -1,18 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand/v2"
+)
 
-func main()  {
-	 a:= [4] int{1,2,3,4}
-	 revers(&a)
-	 fmt.Println(a)
+type account struct {
+	login    string
+	password string
+	url      string
 }
 
-func revers(arr *[4]int )  {
-	fmt.Println((arr)[0])
-	fmt.Println((arr)[1])
-	for index, value := range *arr {
-		(*arr)[len(arr)-1-index] = value
-		fmt.Println(index, value)
+var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890-*!")
+
+func main() {
+	fmt.Println(generatePassword(12))
+
+	login := promtData("Введите логин")
+	password := promtData("Введите пароль")
+	url := promtData("Введите URL")
+
+	myAccount := account{
+		login:    login,
+		password: password,
+		url:      url,
 	}
+	outputPassword(&myAccount)
+
+}
+
+func promtData(promt string) string {
+	fmt.Print(promt + ": ")
+	var res string
+	fmt.Scan(&res)
+	return res
+}
+
+func outputPassword(account *account) {
+	fmt.Println(account.login, account.password, account.url)
+}
+
+func generatePassword(n int) string {
+	
+	rez := make([]rune, n) // итоговый массив (предслайс рун)
+	for i:= range rez {
+		rez[i] = letterRunes[rand.IntN(len(letterRunes))] // любой элемент от длинны заготовленных рун ()
+	}
+	return string(rez)
 }
